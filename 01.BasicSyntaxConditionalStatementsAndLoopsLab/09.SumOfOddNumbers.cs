deb4fff0bfe88925eba9790e0c3c5f3e82df0987
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-09-16 20:44:04 +0300

    Updating Test

--- 01.BasicSyntaxConditionalStatementsAndLoopsLab/09.SumOfOddNumbers.cs
+++ 01.BasicSyntaxConditionalStatementsAndLoopsLab/09.SumOfOddNumbers.cs
@@ -10,14 +10,17 @@ namespace _09.SumOfOddNumbers
     {
         static void Main(string[] args)
         {
-           
+
             int n = int.Parse(Console.ReadLine());
             int sum = 0;
-                for (int i = 1; i <= n * 2; i+=2)
+            for (int i = 1; i <= n * 2; i++)
+            {
+                if (i % 2 == 0)
                 {
-                        Console.WriteLine(i);
-                        sum += i;
+                    Console.WriteLine(i);
+                    sum += i;
                 }
+            }
             Console.WriteLine($"Sum: {sum}");
         }
     }
