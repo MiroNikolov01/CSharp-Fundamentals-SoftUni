7fab5f651ddb1615ce88688027ff3da52200a9cb
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-11-03 00:44:20 +0200

    Add files via upload

---
+++ 04.MethodsLab/07.RepeatString.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputS = Console.ReadLine();
+            int inputNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(RepeatString(inputS, inputNum));
+        }
+        static string RepeatString(string input, int addTimes)
+        {
+            string outPut = input;
+            for (int i = 1; i < addTimes; i++)
+            {
+                outPut += input;
+            }
+            return outPut;
+        }
+    }
+}
