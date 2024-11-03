9f967f08182021dd64fac4fb2bb3b40e68c29492
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-11-03 10:58:27 +0200

    Add files via upload

---
+++ 05.MethodsLab/05.Orders.cs
@@ -0,0 +1,31 @@
+﻿namespace _05.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             string product = Console.ReadLine();
+             decimal quantity = decimal.Parse(Console.ReadLine());
+            OrderProcess(product,quantity);
+        }
+        static void OrderProcess(string product, decimal quantity)
+        {
+             switch (product)
+             {
+                case "coffee": quantity *= 1.5m;
+                    break;
+                case "water":
+                    quantity *= 1.0m;
+                    break;
+                case "coke":
+                    quantity *= 1.40m;
+                    break;
+                case "snacks":
+                    quantity *= 2.0m;
+                    break;
+            }
+                    Console.WriteLine($"{quantity:F2}");
+
+        }
+    }
+}
