c176b089286a2b90ae10a5731bb4c0d9a5ac09d0
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-11-04 11:17:59 +0200

    Update 02.VowelsCount.cs

--- 05.MethodsExercise/02.VowelsCount.cs
+++ 05.MethodsExercise/02.VowelsCount.cs
@@ -1,10 +1,35 @@
 ﻿namespace _02.VowelsCount
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine().ToLower();
+            Console.WriteLine(VowelsCountChecker(input));
+        }
+        static int VowelsCountChecker(string input)
+        {
+            int count = 0;
+            char[] vowels = {
+                'a',
+                'o',
+                'u',
+                'e',
+                'i'
+            };
+            foreach (char c in vowels)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i]
+                        .ToString()
+                        .Contains(c))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
     }
 }
