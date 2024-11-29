687a434e6330b385cc28b41582895dd1e3846e00
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2024-11-29 16:37:46 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals Final Exam/01.WorldTour.cs
@@ -0,0 +1,83 @@
+﻿namespace _01.WorldTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialString = Console.ReadLine();
+            string command;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] tokens = command.Split(":", StringSplitOptions.RemoveEmptyEntries);
+                int startIndex = 0;
+                int endIndex = 0;
+                if (tokens.Length < 2)
+                {
+                    continue;
+                }
+                switch (tokens[0])
+                {
+                    case "Add Stop":
+                        startIndex = int.Parse(tokens[1]);
+                        string insertString = tokens[2];
+                        initialString = Add(startIndex,
+                            insertString,
+                            initialString);
+                        break;
+                    case "Remove Stop":
+                        startIndex = int.Parse(tokens[1]);
+                        endIndex = int.Parse(tokens[2]);
+                        initialString = Remove(startIndex,
+                            endIndex,
+                            initialString);
+                        break;
+                    case "Switch":
+                        string oldString = tokens[1];
+                        string newString = tokens[2];
+                        initialString = Switch(oldString,
+                            newString,
+                            initialString);
+                        break;
+                }
+                Console.WriteLine(initialString);
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {initialString}");
+        }
+
+        private static string Switch(string oldString, string newString, string initialString)
+        {
+            if (initialString.Contains(oldString))
+            {
+                initialString = initialString.Replace(oldString, newString);
+            }
+
+            return initialString;
+        }
+
+        private static string Remove(int startIndex, int endIndex, string initialString)
+        {
+            if (IsValidIndex(startIndex, initialString) && IsValidIndex(endIndex,initialString))
+            {
+                initialString = initialString.Remove(startIndex, endIndex - startIndex + 1);
+            }
+
+            return initialString;
+
+        }
+
+        private static string Add(int index, string insertString, string initialString)
+        {
+            if (IsValidIndex(index, initialString))
+            {
+                initialString = initialString.Insert(index, insertString);
+            }
+
+            return initialString;
+        }
+
+        static bool IsValidIndex(int startIndex, string initialString)
+        {
+            return startIndex >= 0 && startIndex <= initialString.Length - 1;
+        }
+    }
+}
---
+++ 02. Programming Fundamentals Final Exam/03.PlantDiscovery.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _03.PlantDiscovery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Plant> plantsMap = new();
+            int countPlants = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countPlants; i++)
+            {
+                string information = Console.ReadLine();
+                string[] plantTokens = information.Split("<->", StringSplitOptions.RemoveEmptyEntries);
+                Plant plant = new Plant(plantTokens[0],
+                    int.Parse(plantTokens[1]));
+                if (!plantsMap.ContainsKey(plantTokens[0]))
+                {
+                    plantsMap.Add(plantTokens[0], plant);
+                }
+                plantsMap[plantTokens[0]].Update(int.Parse(plantTokens[1]));
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "Exhibition")
+            {
+                string[] commandTokens = command.Split(new string[] { " - ", ": " }, StringSplitOptions.RemoveEmptyEntries);
+                if (!plantsMap.ContainsKey(commandTokens[1]))
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+                switch (commandTokens[0])
+                {
+                    case "Rate":
+                        plantsMap[commandTokens[1]].Rate
+                            (commandTokens[1],
+                            double.Parse(commandTokens[2]),
+                            plantsMap);
+                        break;
+                    case "Update":
+                        plantsMap[commandTokens[1]].Update
+                            (int.Parse(commandTokens[2]));
+                        break;
+                    case "Reset":
+                        plantsMap[commandTokens[1]].Reset
+                            (commandTokens[1], plantsMap);
+                        break;
+                }
+            }
+            Console.WriteLine("Plants for the exhibition:");
+            foreach (var plant in plantsMap.Values)
+            {
+                Console.WriteLine(plant);
+            }
+        }
+    }
+    class Plant
+    {
+        public string Name { get; set; }
+        public int Rarity { get; set; }
+        public List<double> Rating { get; set; } = new List<double>();
+        public Plant(string name, int rarity)
+        {
+            Name = name;
+            Rarity = rarity;
+        }
+        public override string ToString()
+        {
+            var rating = Rating.Count > 0 ? Rating.Average() : 0.00;
+            return $"- {Name}; Rarity: {Rarity}; Rating: {rating:F2}";
+        }
+        public void Update(int rarity)
+        {
+            Rarity = rarity;
+        }
+        public void Rate(string plantName, double rating, Dictionary<string, Plant> plantsMap)
+        {
+            plantsMap[plantName].Rating.Add(rating);
+        }
+        public void Reset(string plantName, Dictionary<string, Plant> plantsMap)
+        {
+            plantsMap[plantName].Rating.Clear();
+        }
+    }
+}
